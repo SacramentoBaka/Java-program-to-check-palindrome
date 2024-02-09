073843352c3ceb7dddc7c2a53c7d9806f05c8591
Sacramento <sacramentomakhubele@gmail.com>
2024-02-09 11:07:56 +0200

    Checking for Palindrome

--- src/Main.java
+++ src/Main.java
@@ -5,52 +5,30 @@ public class Main {
     public static Scanner input = new Scanner(System.in);
 
     public static void main (String[] args) {
-        System.out.println("Hello, Welcome to Fibonacci Sequence number Generator");
-        System.out.println("How Many number you want to be Generated: ");
-        int num = input.nextInt();
-        System.out.println("Okay now i'm printing " + num + " Numbers in the Fibonacci Series");
-        for (int i = 0; i < num; i++) {
-            System.out.print(fibonacciSeries2(i) + " ");
+
+        System.out.print("Enter a Word: ");
+        String originalWord = input.next();
+        originalWord = originalWord.replace(" ", "");
+        String updatedWord = "";
+
+        for (int i = originalWord.length() - 1; i >= 0; i--) {
+            updatedWord += originalWord.charAt(i);
         }
-        System.out.println("[ Index " + num + " contain a value " + fibonacciSeries2(num) + " ].");
-    }
-    public static long fibonacciSeries2 (long n) {
-
-        long firstValue = 0;
-        long secondValue = 1;
-        long nextValue = 1;
-
-        if (n == 0)
-            return firstValue;
-        else if (n == 1)
-            return secondValue;
-        else if (n == 2) {
-            return nextValue;
+        boolean palindrome = true;
+        for (int j = 0; j < originalWord.length(); j++) {
+            if (originalWord.charAt(j) != updatedWord.charAt(j)) {
+                palindrome = false;
+            }
         }
-        for (int i = 3; i <= n; i++) {
-
-            nextValue = firstValue + secondValue;
-            firstValue = secondValue;
-            secondValue = nextValue;
+        if (palindrome) {
+            System.out.println("The Word " + originalWord + " is a Palindrome");
+        } else {
+            System.out.println("The Word " + originalWord + " is not a Palindrome");
         }
-
-        return nextValue;
-    }
-
-
-
-
-
-
-    public static int fibonacciSeries (int n) {
-
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-        return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
     }
 }
+
+
 
 
 
@@ -93,28 +71,38 @@ public class Main {
             *
             *
             *
-               //2. Palindrome Check
-        System.out.print("Enter a Word: ");
-        String originalWord = input.next();
-        originalWord = originalWord.replace(" ", "");
-        String updatedWord = "";
-
-        for (int i = originalWord.length() - 1; i >= 0; i--) {
-            updatedWord += originalWord.charAt(i);
-        }
-
-        boolean palindrome = true;
-
-        for (int j = 0; j < originalWord.length(); j++) {
-            if (originalWord.charAt(j) != updatedWord.charAt(j)) {
-                palindrome = false;
-            }
-        }
-        if (palindrome) {
-            System.out.println("The Word " + originalWord + " is a Palindrome");
-        } else {
-            System.out.println("The Word " + originalWord + " is not a Palindrome");
-        }
-        * */
+*/
 
 
+//
+//    public static void main (String[] args) {
+//        System.out.println("Hello, Welcome to Fibonacci Sequence number Generator");
+//        System.out.println("How Many number you want to be Generated: ");
+//        int num = input.nextInt();
+//        System.out.println("Okay now i'm printing " + num + " Numbers in the Fibonacci Series");
+//        for (int i = 0; i < num; i++) {
+//            System.out.print(fibonacciSeries2(i) + " ");
+//        }
+//        System.out.println("[ Index " + num + " contain a value " + fibonacciSeries2(num) + " ].");
+//    }
+//    public static long fibonacciSeries2 (long n) {
+//
+//        long firstValue = 0;
+//        long secondValue = 1;
+//        long nextValue = 1;
+//
+//        if (n == 0)
+//            return firstValue;
+//        else if (n == 1)
+//            return secondValue;
+//        else if (n == 2) {
+//            return nextValue;
+//        }
+//        for (int i = 3; i <= n; i++) {
+//
+//            nextValue = firstValue + secondValue;
+//            firstValue = secondValue;
+//            secondValue = nextValue;
+//        }
+//        return nextValue;
+//    }
